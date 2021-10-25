56346b219994e0938f592f2ba6ba7b5302fd72d3
LaksCastro <lakscastro@gmail.com>
2021-10-25 02:45:13 -0300

    (#1) Rebuild [CustomScrollView] to update [childCount]

--- lib/widgets/packages_list.dart
+++ lib/widgets/packages_list.dart
@@ -19,53 +19,56 @@ class _PackagesListState extends State<PackagesList>
     with DeviceAppsStoreConsumer, ContextualMenuStoreConsumer {
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(
-      slivers: [
-        const ContextualMenu(),
-        SliverPadding(
-          padding: const EdgeInsets.symmetric(vertical: 6),
-          sliver: SliverList(
-            delegate: SliverChildBuilderDelegate(
-              (context, index) {
-                final current = store.displayableApps[index];
+    return AnimatedBuilder(
+      animation: store,
+      builder: (context, child) => CustomScrollView(
+        slivers: [
+          const ContextualMenu(),
+          SliverPadding(
+            padding: const EdgeInsets.symmetric(vertical: 6),
+            sliver: SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (context, index) {
+                  final current = store.displayableApps[index];
 
-                return PackageTile(current);
-              },
-              childCount: store.displayableApps.length,
+                  return PackageTile(current);
+                },
+                childCount: store.displayableApps.length,
+              ),
             ),
           ),
-        ),
-        SliverList(
-          delegate: SliverChildListDelegate(
-            [
-              Opacity(
-                opacity: 0.1,
-                child: Image.asset('assets/images/bottom.png'),
-              ),
-              DecoratedBox(
-                decoration: BoxDecoration(
-                  boxShadow: [
-                    BoxShadow(
-                      color: kBlack10,
-                      offset: const Offset(k1dp, k1dp),
-                      blurRadius: k1dp / 2,
-                      spreadRadius: k1dp / 2,
-                    ),
-                  ],
-                  color: kCardColor,
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                Opacity(
+                  opacity: 0.1,
+                  child: Image.asset('assets/images/bottom.png'),
                 ),
-                child: DottedBackground(
-                  color: kWhite10,
-                  child: const SizedBox(
-                    height: 10,
-                    width: double.infinity,
+                DecoratedBox(
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        color: kBlack10,
+                        offset: const Offset(k1dp, k1dp),
+                        blurRadius: k1dp / 2,
+                        spreadRadius: k1dp / 2,
+                      ),
+                    ],
+                    color: kCardColor,
+                  ),
+                  child: DottedBackground(
+                    color: kWhite10,
+                    child: const SizedBox(
+                      height: 10,
+                      width: double.infinity,
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }

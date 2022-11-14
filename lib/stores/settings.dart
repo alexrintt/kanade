0395fe61a1823900ad512d0f21f93c9cd925684c
Alex Rintt <alexrintt@gmail.com>
2022-11-14 17:39:33 -0300

    Fix crash when trying to extract apk in a folder that no longer exists

--- lib/stores/settings.dart
+++ lib/stores/settings.dart
@@ -4,17 +4,42 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:shared_storage/saf.dart';
 
 class SettingsStore extends ChangeNotifier {
+  /// Use it for display only features, do not rely on it to create files
+  /// because it can no longer exists if the user deleted.
+  ///
+  /// If you need it to make IO operations call [getExportLocationIfItExists] instead.
   Uri? exportLocation;
+
   late SharedPreferences prefs;
 
   static const kExportLocation = 'exportLocation';
 
+  Future<Uri?> getAndSetExportLocationIfItExists() async {
+    final savedLocationString = prefs.getString(kExportLocation);
+    var savedLocation =
+        savedLocationString?.apply((location) => Uri.parse(location));
+
+    if (savedLocation != null) {
+      final savedLocationExists = await exists(savedLocation) ?? false;
+
+      if (!savedLocationExists) {
+        savedLocation = null;
+      }
+    }
+
+    if (savedLocation == null) {
+      reset();
+    } else {
+      await setExportLocation(savedLocation);
+    }
+
+    return savedLocation;
+  }
+
   Future<void> load() async {
     prefs = await SharedPreferences.getInstance();
 
-    final location = prefs.getString(kExportLocation);
-
-    exportLocation = location?.apply((location) => Uri.parse(location));
+    await getAndSetExportLocationIfItExists();
   }
 
   Future<void> setExportLocation(Uri location) async {
@@ -28,10 +53,17 @@ class SettingsStore extends ChangeNotifier {
   Future<void> requestExportLocation() async {
     final uri = await openDocumentTree(initialUri: exportLocation);
 
-    if (uri != null) await setExportLocation(uri);
+    if (uri != null) {
+      await setExportLocation(uri);
+    } else {
+      // Update the folder if it no longer exists.
+      await getAndSetExportLocationIfItExists();
+    }
   }
 
   Future<void> requestExportLocationIfNotSet() async {
+    final exportLocation = await getAndSetExportLocationIfItExists();
+
     if (exportLocation == null) {
       return requestExportLocation();
     }
